53be3aae4d847353761e84dc9713efe8c3e62d60
jh-tx <jhtx.javascript@gmail.com>
2024-02-12 22:54:40 -0600

    Added date range functionality, and try catch in case date range return no ERA

--- app/search/page.js
+++ app/search/page.js
@@ -6,13 +6,17 @@ import { useState, useEffect } from "react";
 const Search = () => {
   const [input, setInput] = useState("");
   const [name, setName] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
   const [playersID, setPlayerID] = useState("");
   const [playersData, setPlayerData] = useState(null);
-  
+
+
+  // fetch players ID # based on user input
   useEffect(() => {
     const fetchPlayerID = async () => {
         try {
-            const res = await fetch(`https://statsapi.mlb.com/api/v1/people/search?names=${encodeURIComponent(input)}`);
+            const res = await fetch(`https://statsapi.mlb.com/api/v1/people/search?names=${input}`);
             if (!res.ok) {
                 throw new Error("Something went wrong");
             }
@@ -30,12 +34,12 @@ const Search = () => {
     fetchPlayerID();
 }, [input]);
 
-
+  // fetch specific pitcher ERA based on data range provided
   useEffect(() => {
     const fetchData = async () => {
       try {
         if (playersID) {
-          const res = await fetch(`https://statsapi.mlb.com/api/v1/people/${playersID}?hydrate=stats(group=[pitching],type=[byDateRange],startDate=01/01/2023,endDate=10/01/2023,season=2023)`);
+          const res = await fetch(`https://statsapi.mlb.com/api/v1/people/${playersID}?hydrate=stats(group=[pitching],type=[byDateRange],startDate=${startDate},endDate=${endDate})`);
           if (!res.ok) {
             throw new Error("Something went wrong");
           }
@@ -55,6 +59,13 @@ const Search = () => {
     setInput(event.target.value);
   };
 
+  const onStartDateChangeHandler = (event) => {
+    setStartDate(event.target.value);
+  };
+
+  const onEndDateChangeHandler = (event) => {
+    setEndDate(event.target.value);
+  };
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
@@ -62,12 +73,12 @@ const Search = () => {
     // Find user with the input name
     const user = playersData.people.find((playerData) => playerData.fullName.toLowerCase() === input.toLowerCase());
     // If user exists, set the name to input and display the ERA
-    if (user) {
-      setName(`${user.fullName}'s ERA: ${user.stats[0].splits[0].stat.era}`);
-    } else {
-      // If user doesn't exist, display a message
-      setName("Player not found");
+    try{
+      setName(user ? `${user.fullName}'s ERA: ${user.stats[0].splits[0].stat.era}` : "Player not found");
+    }catch(error){
+      setName("No ERA Stat Available For This Date Range")
     }
+    
 
     setInput("");
   };
@@ -76,7 +87,11 @@ const Search = () => {
     <div className={styles.container}>
       <form className={styles.form} onSubmit={onSubmitHandler}>
         <h1>ERA Lookup Tool</h1>
-        <input type="text" placeholder="pitchers name.." value={input} name="pitcherName" onChange={onChangeHandler} className={styles.input} />
+        <label htmlFor="startDate" className={styles.label}>Start Date:</label>
+        <input type="date" id="startDate" className={styles.dates} name="startDate" min="1900-01-01" onChange={onStartDateChangeHandler} max="2024-12-31" required/>
+        <label htmlFor="endDate" className={styles.label}>End Date:</label>
+        <input type="date" id="endDate"  className={styles.dates} name="endDate" min="1900-01-01" onChange={onEndDateChangeHandler} max="2024-12-31" required/>
+        <input type="text" placeholder="pitchers name.." value={input} name="pitcherName" onChange={onChangeHandler} className={styles.input} required/>
         <button type="submit" className={styles.button}>Search</button>
         <h2>{name}</h2>
       </form>
