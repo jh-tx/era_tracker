70c1a8b0f3b444fb63b4b8bb7e7e42d326e7e4be
jh-tx <jhtx.javascript@gmail.com>
2024-02-20 21:12:47 -0600

    removed useless items

---
+++ app/login/page.jsx
@@ -0,0 +1,17 @@
+import styles from "@/app/ui/login/login.module.css"
+//import { authenticate } from "../lib/actions";
+
+const loginPage = () => {
+    return (
+        <div className={styles.container}>
+            <form action="" className={styles.form}>
+                <h1>Login</h1>
+                <input type="text" placeholder="username" name="username"/>
+                <input type="password" placeholder="password" name="password"/>
+                <button className={styles.button}>Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default loginPage;
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: "img.mlbstatic.com",
+            }
+
+        ]
+    }
 };
 
 export default nextConfig;
